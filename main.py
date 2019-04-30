e64747128d906ddeb2cfec2c773552e688f03d27
Sam Lea <sam.lea@aviatnet.com>
2019-04-30 21:57:04 +1200

    Update main and lib structure to get a feature class instance (v1 and v2)

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
+import importlib
+
+
+class Device(object):
+    def __init__(self, version):
+        mod = importlib.import_module(
+            "multi_ver_lib.{}.feature".format(version)
+        )
+        print(mod)
+        self.feature = mod.get_feature(version)
+
+
 def main():
-    from multi_ver_lib.v1_0_0.feature import get_something as get_something_v1
-    from multi_ver_lib.v2_0_0.feature import get_something as get_something_v2
-    print(get_something_v1())
-    print(get_something_v2())
+    v1_device = Device("v1_0_0")
+    v2_device = Device("v2_0_0")
+
+    v1_device.feature.set_id(3)
+    print(v1_device.feature.get_id())
+    print(v2_device.feature.get_id())
+
+    v2_device.feature.set_id(2)
+    print(v1_device.feature.get_id())
+    print(v2_device.feature.get_id())
 
 
 if __name__ == "__main__":
