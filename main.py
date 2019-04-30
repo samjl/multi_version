9ff4d788a18acbe86d34a9c6080b54c58bb54bdb
Sam Lea <sam.lea@aviatnet.com>
2019-04-30 20:41:06 +1200

    Add v2 lib submodule and import from v1 and v2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
-    from multi_ver_lib.v1_0_0.feature import get_something
-    print(get_something())
+    from multi_ver_lib.v1_0_0.feature import get_something as get_something_v1
+    from multi_ver_lib.v2_0_0.feature import get_something as get_something_v2
+    print(get_something_v1())
+    print(get_something_v2())
 
 
 if __name__ == "__main__":
